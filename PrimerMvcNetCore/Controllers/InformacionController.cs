dd51a45a845aff5a6237ceae14ecfb113e4f594c
serraguti <pacoserranox@gmail.com>
2023-02-03 10:02:31 +0100

    commit

---
+++ PrimerMvcNetCore/Controllers/InformacionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ControladorVista()
+        {
+            ViewData["NOMBRE"] = "Alumno";
+            ViewBag.Nombre = "Super Alumno";
+            ViewData["EDAD"] = 27;
+            return View();
+        }
+    }
+}
