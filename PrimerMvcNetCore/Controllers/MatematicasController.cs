a35e3da3446182996c47a80fd78958fa155dcbc9
serraguti <pacoserranox@gmail.com>
2023-02-06 11:12:17 +0100

    commit

--- PrimerMvcNetCore/Controllers/MatematicasController.cs
+++ PrimerMvcNetCore/Controllers/MatematicasController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PrimerMvcNetCore.Models;
 
 namespace PrimerMvcNetCore.Controllers
 {
@@ -7,6 +8,66 @@ namespace PrimerMvcNetCore.Controllers
         public IActionResult Menu()
         {
             return View();
+        }
+
+        public IActionResult TablaMultiplicarSimple()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TablaMultiplicarSimple(int valor)
+        {
+            List<int> resultados = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                int operacion = valor * i;
+                resultados.Add(operacion);
+            }
+            return View(resultados);
+        }
+
+        public IActionResult TablaMultiplicarCompuesta()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TablaMultiplicarCompuesta(int numero)
+        {
+            List<FilaTablaMultiplicar> resultados = new List<FilaTablaMultiplicar>();
+            for (int i = 1; i <= 10; i++)
+            {
+                FilaTablaMultiplicar fila = new FilaTablaMultiplicar();
+                fila.Operacion = numero + " * " + i;
+                fila.Resultado = numero * i;
+                resultados.Add(fila);
+            }
+            return View(resultados);
+        }
+
+        public IActionResult ConjeturaCollatz()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConjeturaCollatz(int numero)
+        {
+            List<int> numeros = new List<int>();
+            while (numero != 1)
+            {
+                if (numero % 2 == 0)
+                {
+                    numero = numero / 2;
+                }
+                else
+                {
+                    numero = numero * 3 + 1;
+                }
+                numeros.Add(numero);
+            }
+            return View(numeros);
         }
 
         //TENDREMOS UNA VISTA QUE RECIBIRA LOS DATOS 
